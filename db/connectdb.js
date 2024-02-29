5c946c59f077410f44955b7780ea14325af66c26
Saurav Bharti <87286258+sau45@users.noreply.github.com>
2024-02-29 21:41:30 +0530

    Update connectdb.js

--- db/connectdb.js
+++ db/connectdb.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 
 
 const pool = new Pool({
-    connectionString: "postgres://default:LM6JX3xWgtri@ep-orange-snow-a1yqijtl-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require",
+    connectionString: "",
 })
 
 try {
